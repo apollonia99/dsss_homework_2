03580460bb1ad0fc6a185f6828e76aaa86d04ac8
Sarah Keck <78654233+apollonia99@users.noreply.github.com>
2024-11-08 16:02:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='math_quiz',
     version='0.1',
-    packages=['random', 'unittest',],
-    long_description=open('README.md').read()
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'math_quiz=math_quiz.math_quiz:main',
+        ],
+    }
 )
